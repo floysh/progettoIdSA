3b7699538a71274b401aa971980c3b7fa0b30b87
Davide Civolani <davide01.civolani@edu.unife.it>
2022-07-14 19:28:38 +0200

    fix tabella users

---
+++ database/migrations/2022_07_14_170113_add_role_money_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->float('money')->default(1500);
+            $table->enum('role', array('merchant', 'customer'))->default('customer');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('money');
+            $table->dropColumn('role');
+        });
+    }
+};
