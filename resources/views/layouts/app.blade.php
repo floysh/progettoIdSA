c2df45cecc76d0a1efb3177f04326e0575b2b54e
Davide Civolani <davide01.civolani@edu.unife.it>
2022-08-20 22:43:04 +0200

    installato fontawesome (npm/mix)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,19 +16,8 @@
     
     @yield('page-javascript')
 
-
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    {{-- <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css"> --}}
-    {{-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous"> --}}
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
-    {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"/> --}}
-    {{-- <link href='https://fonts.googleapis.com/css?family=Product+Sans' rel='stylesheet' type='text/css'> --}}
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
 
     @yield('page-css')
 
