947fe73bc5b68b882526c8853341a3a26c4cf009
Davide Civolani <davide01.civolani@edu.unife.it>
2022-08-22 17:45:41 +0200

    bulma: order show

--- resources/views/Order/index.blade.php
+++ resources/views/Order/index.blade.php
@@ -26,43 +26,7 @@
     @foreach($orders as $order)
       <div class="list">
         <div class="list-item mb-6">
-          <h4 class="title is-size-4">Ordine # {{$order->id}}</h4>
-          {{-- Info --}}
-          <nav class="level mb-0">
-            <div class="level-item block">
-              <div>
-                <p class="heading">Effettuato il</p>
-                <p class="title is-size-5">{{ $order->created_at }}</p>
-              </div>
-            </div>
-            <div class="level-item has-text-right">
-              <div>
-                <p class="heading">Prodotti acquistati</p>
-                <p class="title is-size-5">{{ $order->products()->count() }}</p>
-              </div>
-            </div>
-          </nav>
-          {{-- Elenco prodotti --}}
-          <div class="content">
-            <table class="table">
-              <thead>
-                <th>Quantità</th>
-                <th>Prodotto</th>
-                <th>Mercante</th>
-              </thead>
-              <tbody>
-                @foreach ($order->products as $product)
-                  <tr>
-                    <td>{{ $product->order_properties->quantity }}x</td>
-                    <td>
-                      <a href="{{ action('ProductController@show',$product) }}">{{ $product->name }}</a>
-                    </td>
-                    <td>$product->merchant->name </td>
-                  </tr>
-                @endforeach
-              </tbody>
-            </table>
-          </div>
+          @include('Order._order')
         </div>
       </div>
     @endforeach
--- resources/views/Order/show.blade.php
+++ resources/views/Order/show.blade.php
@@ -5,20 +5,7 @@
 @endsection
 
 @section('content')
-    <div>
-    <h3>Ordine n.{{ $order->id }}</h3>
-    <h4>{{ $order->total() }} soldini</h4>
-    </div>
-    <div>
-    @foreach($order->products as $product)
-    <div>
-        <ul>
-            <li><a href="{{ action('ProductController@show',$product->id) }}">{{ $product->name }}</a></li>
-            <li><img src="{{ $product->imgpath }}.png" alt="{{ $product->imgpath }}"></li>
-            <li>{{ $product->order_properties->quantity }} x {{ $product->price }} soldini</li>
-            <li>{{ $product->description }}</li>
-        </ul>
-    </div>
-    @endforeach
+    <div class="section">
+        @include('Order._order')
     </div>
 @endsection
