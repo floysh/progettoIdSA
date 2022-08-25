ad479f029b78f8f2bea680233576379037627cca
Davide Civolani <davide01.civolani@edu.unife.it>
2022-08-25 16:57:11 +0200

    refactoring: home spotlight

--- resources/views/_spotlight.blade.php
+++ resources/views/_spotlight.blade.php
@@ -1,24 +1,14 @@
 <a href="{{ $url ?? '#'}}">
-    <div class="notification" 
-        style="
-            background-image: url('{{ $backgroundImage ?? '/'}}');
-            box-shadow: inset 0 -27px 115px -28px black;
-            background-repeat: no-repeat;
-            background-size: 100%;
-            ">
-        <div class="hero">
-            <div class="hero-body"></div>
-            <div class="hero-body"></div>
-            <div class="hero">
-                <div class="content has-text-white is-hidden-mobile">
-                    <div class="title is-size-4 is-size-5-mobile">{{ $title ?? Faker\Factory::create()->sentence() }}</div>
-                    <div class="subtitle is-size-5 is-size-6-mobile mb-3">{{ $subtitle ?? Faker\Factory::create()->sentence() }}</div>
-                    <div class="button is-outlined">Visualizza dettagli</div>
-                </div>
+    <div class="spotlight has-background-dimmed" style="background-image: url('https://i.pinimg.com/originals/ee/0a/47/ee0a47d7342f4ab1db5590793875d53c.png')">
+        <div class="spotlight-content">
+            <div class="content has-text-white is-hidden-mobile">
+                <div class="title is-size-4 is-size-5-mobile">{{ $title ?? Faker\Factory::create()->sentence() }}</div>
+                <div class="subtitle is-size-5 is-size-6-mobile mb-3">{{ $subtitle ?? Faker\Factory::create()->sentence() }}</div>
+                <div class="button is-outlined">Visualizza dettagli</div>
             </div>
         </div>
     </div>
-    <div class="content has-text-white is-hidden-tablet">
+    <div class="content mt-5 has-text-white is-hidden-tablet">
         <div class="button is-pulled-right" style="border: none">
             <i class="fas fa-arrow-right"></i>
         </div>
